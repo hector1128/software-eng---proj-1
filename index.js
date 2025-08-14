f576a6037eef884f0b71235184cde0c629412eb6
hector1128 <hectorhoraciocordero11@gmail.com>
2025-08-14 10:06:43 -0400

    Succesfuly added the button logic + css design for it. When I click each box, a new message appears in the page

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
-const aboutmeSection = document.querySelector(".aboutmeSec");
+const tali = document.querySelector(".tali");
 
-if (aboutmeSection) {
-  aboutmeSection.addEventListener("click", aboutmeMessage);
+const UCF = document.querySelector(".UCF");
+
+const gettingRich = document.querySelector(".gettingRich");
+
+const exploration = document.querySelector(".exploration");
+
+const responseBox = document.querySelector(".aboutmeSecResponse");
+
+function toggleAboutMe(aboutmeSectionName) {
+  if (aboutmeSectionName === tali) {
+    aboutmeSectionName.addEventListener("click", () => {
+      document.querySelector(".aboutmeSecResponse").innerHTML =
+        "Tali is a great person!";
+      responseBox.style.display = "block";
+    });
+  }
+
+  if (aboutmeSectionName === UCF) {
+    aboutmeSectionName.addEventListener("click", () => {
+      document.querySelector(".aboutmeSecResponse").innerHTML = "I love UCF!";
+      responseBox.style.display = "block";
+    });
+  }
+
+  if (aboutmeSectionName === gettingRich) {
+    aboutmeSectionName.addEventListener("click", () => {
+      document.querySelector(".aboutmeSecResponse").innerHTML = "shmone!";
+      responseBox.style.display = "block";
+    });
+  }
+
+  if (aboutmeSectionName === exploration) {
+    aboutmeSectionName.addEventListener("click", () => {
+      document.querySelector(".aboutmeSecResponse").innerHTML =
+        "internet explorer!";
+      responseBox.style.display = "block";
+    });
+  }
 }
 
-function aboutmeMessage() {
-  console.log("About Me section clicked!");
-}
+toggleAboutMe(tali);
+
+toggleAboutMe(UCF);
+
+toggleAboutMe(gettingRich);
+
+toggleAboutMe(exploration);
